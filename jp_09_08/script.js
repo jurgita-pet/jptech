ec0282aac747967914c5d5adea8c2308841fb2d6
jurgita-pet <jurgita5@hotmail.com>
2022-08-11 15:28:18 +0300

    input of 2 values and operation buttons. v2

--- jp_09_08/script.js
+++ jp_09_08/script.js
@@ -1,27 +1,17 @@
-function addition() {
+function operation(action) {
     const value1 = Number(document.getElementById("value1").value);
     const value2 = Number(document.getElementById("value2").value);
-    let result = value1 + value2;
-    document.getElementById('calculationResult').innerText = result;
-}
-
-function substraction() {
-    const value1 = Number(document.getElementById("value1").value);
-    const value2 = Number(document.getElementById("value2").value);
-    let result = value1 - value2;
-    document.getElementById('calculationResult').innerText = result;
-}
-
-function multiplication() {
-    const value1 = Number(document.getElementById("value1").value);
-    const value2 = Number(document.getElementById("value2").value);
-    let result = value1 * value2;
-    document.getElementById('calculationResult').innerText = result;
-}
-
-function division() {
-    const value1 = Number(document.getElementById("value1").value);
-    const value2 = Number(document.getElementById("value2").value);
-    let result = value1 / value2;
+    let result = 0;
+    if (action === "addition") {
+        result = value1 + value2;
+    } else if (action === "substraction") {
+        result = value1 - value2;
+    } else if (action === "multiplication") {
+        result = value1 * value2;
+    } else if (action === "division") {
+        result = value1 / value2;
+    } else {
+        alert("Error.");
+    }
     document.getElementById('calculationResult').innerText = result;
 }
