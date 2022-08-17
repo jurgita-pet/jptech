d2457dc11bc1799e436444adaffe0b8f75db68db
jurgita-pet <jurgita5@hotmail.com>
2022-08-17 13:22:51 +0300

    table with filter correct

--- jp_11_08/script.js
+++ jp_11_08/script.js
@@ -1,31 +1,59 @@
-function filterColor() {
-    let filteredItem = document.getElementById("colors");
-    document.getElementById("filteredColor").innerHTML = filteredItem.options[filteredItem.selectedIndex].text;
-    let colorList = document.getElementsByClassName('color');
-    for (let i = 0; i < colorList.length; i++) {
-        colorList[i].style.visibility = 'hidden';
+function filter() {
+    let brandValue = document.getElementById("brandFilter");
+    let colorValue = document.getElementById("colorFilter");
+    let allRows = document.getElementById("tableRows").getElementsByTagName("tr");
+    for (let i = 0; i < allRows.length; i++) {
+        let row = allRows[i];
+        let rowBrandVal = row.getElementsByTagName("td")[0].innerText;
+        let rowColorVal = row.getElementsByTagName("td")[1].innerText;
+        if ((brandValue.value === "" || brandValue.selectedOptions[0].innerText === rowBrandVal) && (colorValue.value === "" || colorValue.selectedOptions[0].innerText === rowColorVal)) {
+            row.hidden = false;
+        } else {
+            row.hidden = true;
+        }
     }
 }
 
-function filterFlower() {
-    let filteredItem = document.getElementById("flowers");
-    document.getElementById("filteredFlower").innerHTML = filteredItem.options[filteredItem.selectedIndex].text;
-    let flowerList = document.getElementsByClassName('flower');
-    for (let i = 0; i < flowerList.length; i++) {
-        flowerList[i].style.visibility = 'hidden';
-    }
-}
-
-function addNewElement() {
-    let table = document.getElementById("table");
-    let newRow = table.insertRow(-1);
-    let cell1 = newRow.insertCell(0);
-    let cell2 = newRow.insertCell(1);
-    let addNewColor = document.getElementById("newColor").value;
-    let addNewFlower = document.getElementById("newFlower").value;
-    cell1.innerHTML = addNewColor;
-    cell2.innerHTML = addNewFlower;
+function addNewRow() {
+    let newBrandVal = document.getElementById("newBrand").value;
+    let newColorVal = document.getElementById("newColor").value;
+    let newRowElement = document.createElement("tr");
+    let newBrandColumn = document.createElement("td");
+    newBrandColumn.innerText = newBrandVal;
+    let newColColumn = document.createElement("td");
+    newColColumn.innerText = newColorVal;
+    newRowElement.appendChild(newBrandColumn);
+    newRowElement.appendChild(newColColumn);
+    document.getElementById("tableRows").appendChild(newRowElement);
+    addNewBrandFilter(newBrandVal);
+    addNewColorFilter(newColorVal);
     clearInputs();
+}
+
+function addNewBrandFilter(brand) {
+    let brandFilter = document.getElementById("brandFilter");
+    let allSelectOption = brandFilter.getElementsByTagName("option");
+    for (let i = 0; i < allSelectOption.length; i++) {
+        if (allSelectOption[i].innerText === brand) {
+            return;
+        }
+    }
+    let newSelectionOption = document.createElement("option");
+    newSelectionOption.innerText = brand;
+    brandFilter.appendChild(newSelectionOption);
+}
+
+function addNewColorFilter(color) {
+    let colorFilter = document.getElementById("colorFilter");
+    let allSelectOption = colorFilter.getElementsByTagName("option");
+    for (let i = 0; i < allSelectOption.length; i++) {
+        if (allSelectOption[i].innerText === color) {
+            return;
+        }
+    }
+    let newSelectionOption = document.createElement("option");
+    newSelectionOption.innerText = color;
+    colorFilter.appendChild(newSelectionOption);
 }
 
 function clearInputs() {
