b4f9c1a82e25184b41c8cc3008ae96db2825c23b
jurgita-pet <jurgita5@hotmail.com>
2022-08-21 21:44:24 +0300

    intro to node.js

---
+++ jp_18_08/backend/index.js
@@ -0,0 +1,35 @@
+const exppress = require("express");
+const app = exppress();
+const fs = require("fs");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/get-allCustomer", (req, res) => {
+    fs.readFile('AllCustomers.json', "utf8", (err, data) => {
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": JSON.parse(data) }))
+    })
+})
+
+app.post("/add-customer", (req, res) => {
+    const newCustomer = {
+        "firstname": req.body.name,
+        "lastname": req.body.lastname,
+        "phone": req.body.email,
+        "email": req.body.phone
+    }
+    fs.readFile('AllCustomers.json', "utf8", (err, data) => {
+        const customers = JSON.parse(data);
+        customers.customers.push(newCustomer);
+        fs.writeFile('AllCustomers.json', JSON.stringify(customers, null, 3), function (err) {
+            res.send("Customer is added");
+        });
+    })
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+})
